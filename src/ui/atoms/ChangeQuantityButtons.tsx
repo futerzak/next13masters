ab7b4580a683531a757989a0dcad92f0d8f1de16
Michał Topór-Futer <futerzak@gmail.com>
2023-10-10 23:26:25 +0200

    feat(week-4): rollback packages versions

--- src/ui/atoms/ChangeQuantityButtons.tsx
+++ src/ui/atoms/ChangeQuantityButtons.tsx
@@ -1,5 +1,5 @@
 "use client";
-import { useOptimistic } from "react";
+import { experimental_useOptimistic as useOptimistic } from "react";
 import { changeItemQuantity } from "@/app/cart/actions";
 
 
